8c4d357f658c7d0dd98e2cb051bee77850cc8136
Rafael <rafaelsilva.fatec@gmail.com>
2018-07-16 22:41:11 -0300

    Create address reducer

---
+++ src/redux-flow/reducers/address/address.test.js
@@ -0,0 +1,42 @@
+import { expect } from 'chai'
+import deepFreeze from 'deep-freeze'
+import address from './index'
+import { UPDATE_ADDRESS } from './actions'
+
+it('address should be a function', () => {
+    expect(address).to.be.a('function')
+})
+
+it('should action UPDATE_ADDRESS update address', () => {
+    const before = deepFreeze({
+        address: '',
+        city: '',
+        code: '',
+        district: '',
+        state: '',
+        status: 1,
+    })
+
+    const action = deepFreeze({
+        type: UPDATE_ADDRESS,
+        payload: {
+            address: 'Rua Japurá',
+            city: 'Rio de Janeiro',
+            code: '21320-000',
+            district: 'Praça Seca',
+            state: 'RJ',
+            status: 1,
+        }
+    })
+
+    const after = {
+        address: 'Rua Japurá',
+        city: 'Rio de Janeiro',
+        code: '21320-000',
+        district: 'Praça Seca',
+        state: 'RJ',
+        status: 1,
+    }
+
+    expect(address(before, action)).to.be.deep.equal(after)
+})
