e1729a9cde1809808ec6d86927beb904846149ef
Rafael <rafaelsilva.fatec@gmail.com>
2018-07-25 22:12:45 -0300

    Add redux-thunk for async dispatch

--- src/index.js
+++ src/index.js
@@ -3,38 +3,27 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import { devToolsEnhancer } from 'redux-devtools-extension'
 import App from './app'
-import reducer from 'reducers'
+import configureStore from './redux-flow/configure-store'
 
-const initialState = {
-  todos: [{
-    text: 'auto',
-    id: '123',
-    completed: false
-  }],
-  address: {
-    status: 1,
-    code: "03380-070",
-    state: "SP",
-    city: "São Paulo",
-    district: "Chácara Belenzinhoooo",
-    address: "Rua Sexto Empírico",
+const store = configureStore({
+  initialState : {
+    todos: [{
+      text: 'auto',
+      id: '123',
+      completed: false
+    }],
+    address: {
+      status: 1,
+      code: "03380-070",
+      state: "SP",
+      city: "São Paulo",
+      district: "Chácara Belenzinhoooo",
+      address: "Rua Sexto Empírico",
+    }
   }
-}
-
-const logger = ({ dispatch, getState }) => (next) => (action) => {
-  console.log('LOGGER:: will dispatch', action)
-  const nextAction = next(action)
-  console.log('LOGGER:: next action', nextAction)
-  return nextAction
-}
-
-const store = createStore(reducer, initialState, applyMiddleware(logger), devToolsEnhancer(
-  // Specify custom devTools options
-))
+})
 
 const renderApp = (NextApp) => {
   render(
@@ -53,10 +42,5 @@ if (module.hot) {
   module.hot.accept('./app', () => {
     const NextApp = require('./app').default
     renderApp(NextApp)
-  })
-
-  module.hot.accept('reducers', () => {
-    const nextReducer = require('reducers').default
-    store.replaceReducer(nextReducer)
   })
 }
---
+++ src/redux-flow/configure-store/index.js
@@ -0,0 +1,28 @@
+import { createStore, applyMiddleware } from 'redux'
+import { devToolsEnhancer } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import rootReducer from 'reducers'
+
+export default ({ initialState } = {}) => {
+    const store = createStore(rootReducer, initialState, applyMiddleware(logger, thunk), devToolsEnhancer(
+        // Specify custom devTools options
+    ))
+
+    if (module.hot) {
+        module.hot.accept('reducers', () => {
+            const nextReducer = require('reducers').default
+            store.replaceReducer(nextReducer)
+        })
+    }
+    return store
+}
+
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+    console.group(`LOGGER->${action.type}`)
+    console.log('will dispatch', action)
+    console.log('state', getState())
+    const nextAction = next(action)
+    console.log('next state', getState())
+    console.groupEnd(`LOGGER->${action.type}`)
+    return nextAction
+}
