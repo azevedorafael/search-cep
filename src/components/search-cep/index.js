5dbfbaa9c35ba6a04c73f63115b02177ee6333bd
Rafael <rafaelsilva.fatec@gmail.com>
2018-08-24 23:24:36 -0300

    Add async actions

--- src/components/search-cep/index.js
+++ src/components/search-cep/index.js
@@ -1,44 +1,14 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import SearchCep from './search-cep'
-import { get } from 'axios'
-import { updateAddress } from 'reducers/address/action-creators'
+import { fetchAddress } from 'reducers/address/action-creators'
 
-class SearchCepContainer extends PureComponent {
-  constructor() {
-    super()
-    this.state = {
-      isFetching: false
-    }
-  }
-
-  //  Auto binding due Arrow Function this scope lexical  
-  handleSubmit = async (e) => {
-    e.preventDefault()
-    this.setState({ isFetching: true })
-    const cep = e.target.cep.value
-    await get(`http://apps.widenet.com.br/busca-cep/api/cep.json?code=${cep}`)
-      .then((response) => {
-        // handle success
-        this.setState({ isFetching: false })
-        this.props.updateAddress(response.data)
-      })
-      .catch((error) => {
-        // handle error
-        this.setState({ isFetching: false })
-      })
-  }
-
-  render() {
-    return (
-      <SearchCep
-        {...this.state}
-        {...this.props.address}
-        handleSubmit={this.handleSubmit}
-      />
-    )
-  }
-}
+const SearchCepContainer = ({ address, handleSubmit }) => (
+  <SearchCep
+    {...address}
+    handleSubmit={handleSubmit}
+  />
+)
 
 const mapStateToProps = (state) => ({
   address: state.address
@@ -48,6 +18,11 @@ const mapStateToProps = (state) => ({
 //   updateAddress: (data) => dispatch(updateAddress(data))
 // })
 
-const mapDispatchToProps = { updateAddress }
+const mapDispatchToProps = (dispatch) => ({
+  handleSubmit: (e) => {
+    e.preventDefault()
+    dispatch(fetchAddress(e.target.cep.value))
+  }
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchCepContainer)
--- src/redux-flow/reducers/address/action-creators.js
+++ src/redux-flow/reducers/address/action-creators.js
@@ -1,4 +1,17 @@
 import { UPDATE_ADDRESS } from "./actions";
+import { get } from 'axios'
+
+export const fetchAddress = (cep) => async (dispatch, getState) => {
+    await get(`http://apps.widenet.com.br/busca-cep/api/cep.json?code=${cep}`)
+        .then((response) => {
+            // handle success
+            dispatch(updateAddress(response.data))
+        })
+        .catch((error) => {
+            // handle error
+            this.setState({ isFetching: false })
+        })
+}
 
 export const updateAddress = (data) => ({
     type: UPDATE_ADDRESS,
